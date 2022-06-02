e57b844a49587f82a764417f6dc124a83c1a219c
HidyHan <hidyhan5@gmail.com>
2022-06-02 21:11:05 +0000

    Uploads working hyper params.

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python3 train_xsa_e2e.py --savedir new_model/ --train train.txt --test dev.txt --seed 42 --lang 2 --model_ckpt Transformer_89.0304_5.4848.ckpt --lr 0.00001 --epochs 10
+python3 train_xsa_e2e.py --savedir new_model/ --train train_original.txt --test dev.txt --seed 42 --lang 2 --model_ckpt Transformer_89.0304_5.4848.ckpt --lr 0.0001 --epochs 50 --batch 16 --maxlength 1000
