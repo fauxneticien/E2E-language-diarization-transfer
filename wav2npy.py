999b0c3ccca1e686fbba43ed660e9d816dd3c436
Hidy Han <hidyhan5@gmail.com>
2022-06-03 18:37:12 -0700

    Supports ecapa-tdnn feature extraction in wav2npy.

--- wav2npy.py
+++ wav2npy.py
@@ -1,9 +1,13 @@
+import argparse
 import os
 import math
 import python_speech_features as psf
 import scipy.io.wavfile as wav
+import torchaudio
 import numpy as np
 import glob
+
+from speechbrain.pretrained import EncoderClassifier
 
 # Liu et al: https://www.isca-speech.org/archive/pdfs/interspeech_2021/liu21d_interspeech.pdf
 
@@ -15,15 +19,25 @@ def extract_fbank(signal, sr, dim=23, log=True):
         features = psf.fbank(signal, sr, nfilt=dim, lowfreq=20, highfreq=high_frequency)
     return features
 
-#wav_files = glob.glob("binary_processed/*.wav")
-wav_files = glob.glob("augmented_data/*.wav")
-# wav_files = ["4oLp3bc9OSJbDrwM_0000.wav"]
-if not os.path.isdir('binary_npys'):
-    os.mkdir("binary_npys")
 
-for wav_file in wav_files:
+def get_wav_features(wav_file, feature, language_id):
+    wav_sr = 16000
+    wav_signal = None
+    ft_dim = 0
+    if feature == 'fbank':
+        (wav_sr, wav_signal) = wav.read(wav_file)
+        # Liu et al: "For each segment 23-dimensional log-Mel-filterbank features are extracted with 25 ms window and a 10 ms shift as [10] for all systems."
 
-    (wav_sr, wav_signal) = wav.read(wav_file)
+        # 10 ms shift over 200 ms  = 19 frames
+        # 19 frames x 23-dim feats = 437 total features
+        ft_dim = 437
+
+    elif feature == 'etdnn':
+        wav_signal = language_id.load_audio(wav_file)
+        ft_dim = 256
+    else:
+        print('Unrecognized feature type: ', feature)
+        return
 
     # clip duration in seconds
     wav_dur = len(wav_signal) / wav_sr
@@ -37,30 +51,54 @@ for wav_file in wav_files:
     # Liu et al: "We ignore the remaining of the speech samples that cannot be divided exactly into 200ms segments."
     n_parts  = math.floor(n_parts)
 
-    # Liu et al: "For each segment 23-dimensional log-Mel-filterbank features are extracted with 25 ms window and a 10 ms shift as [10] for all systems."
-
-    # 10 ms shift over 200 ms  = 19 frames
-    # 19 frames x 23-dim feats = 437 total features
-    #
-    # create placeholder of shape (p, 437), where p = number of partitions
-    features = np.zeros((n_parts, 437))
+    features = np.zeros((n_parts, ft_dim))
 
     for ith_part in range(n_parts):
 
         start_sample = int(ith_part * 0.2 * wav_sr)
-        end_sample   = int((start_sample + (0.2 * wav_sr) - 1))
+        end_sample   = int(start_sample + (0.2 * wav_sr))
 
         part_signal   = wav_signal[start_sample:end_sample]
-        part_features = extract_fbank(part_signal, wav_sr)
+        part_features = None
+        if feature == 'fbank':
+            part_features = extract_fbank(part_signal, wav_sr)
+        elif feature == 'etdnn':
+            part_features = language_id.encode_batch(part_signal)
 
         # Crop-frame in case overflow
-        cropped_frame = part_features.shape[0] % 19
+        #cropped_frame = part_features.shape[0] % 19
 
-        if cropped_frame != 0:
-            part_features = part_features[:-cropped_frame, :]
+        #if cropped_frame != 0:
+        #    part_features = part_features[:-cropped_frame, :]
 
         features[ith_part,] = part_features.reshape(-1)
+    return features
 
-    # Save feature file
-    npy_name = os.path.join("binary_npys", os.path.basename(wav_file).rsplit('.')[0] + '.npy')
-    np.save(npy_name, features)
+def main():
+    parser = argparse.ArgumentParser(description='params')
+    parser.add_argument('--savedir', type=str, default='new_npys')
+    parser.add_argument('--feature', type=str, help='etdnn or fbank', default='fbank')
+
+    args = parser.parse_args()
+
+    wav_files = glob.glob("binary_processed/*.wav")
+    #wav_files = glob.glob("augmented_data/*.wav")
+    #wav_files = ["binary_processed/4oLp3bc9OSJbDrwM_0022.wav"]
+
+    # savedir can be e.g. binary_npys_ecapa_tdnn
+    if not os.path.isdir(args.savedir):
+        os.mkdir(args.savedir)
+
+    # Only used for ecapa-tdnn.
+    language_id = EncoderClassifier.from_hparams(source="speechbrain/lang-id-voxlingua107-ecapa", savedir="tmp")
+
+    for wav_file in wav_files:
+        features = get_wav_features(wav_file, args.feature, language_id)
+        # Save feature file
+        npy_name = os.path.join(args.savedir, os.path.basename(wav_file).rsplit('.')[0] + '.npy')
+        np.save(npy_name, features)
+
+
+
+if __name__ == "__main__":
+    main()
